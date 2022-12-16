17c68841d94f417292ce2c39110945e8ef8c97f5
rafaelpm20 <83605454+rafaelpm20@users.noreply.github.com>
2022-12-16 10:24:29 -0300

    Add files via upload

---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,13 @@
+<?php
+    include_once "menu.php"; // CASO TENTE ACESSAR UM ARQUIVO QUE NAO EXISTE, EMITE APENAS UM WARNING
+
+    //require "menu.php";// CASO TENTE ACESSAR UM ARQUIVO QUE NAO EXISTE, EMITE UM FATAL ERRO
+    //require_once "menu.php";
+?>
+conteúdo da página (inicio)
+<br/>
+<?php
+    include_once "menu.php"; // CASO TENTE ACESSAR UM ARQUIVO QUE NAO EXISTE, EMITE APENAS UM WARNING
+
+    //require "menu.php";// CASO TENTE ACESSAR UM ARQUIVO QUE NAO EXISTE, EMITE UM FATAL ERRO
+?>
